bd8b673bb548640d3a5512808d012db5d9381637
John Kang <pollyfactor@hotmail.com>
2020-03-26 17:38:33 -0400

    added initial prompt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const axiodd = require("axios");
+const axios = require("axios");
 
 const questions = ["what is your Github username?", 
 "What is your project title?", 
@@ -12,13 +12,64 @@ const questions = ["what is your Github username?",
 "What does the user need to know about contributing to the repo?"
 ];
 
+inquirer.prompt({
+        type: "input",
+        name: "username",
+        message: questions[0]
+    }).then(function({username}) {
+        console.log(username);
+      });     
+
+// inquirer.prompt([
+//     {
+//       type: "input",
+//       name: "githubName",
+//       message: questions[0]
+//     },
+//     {
+//         type: "input",
+//         name: "description",
+//         message: questions[0]
+//       },
+    
+//     {
+//       type: "checkbox",
+//       message: "What languages do you know?",
+//       name: "stack",
+//       choices: [
+//         "HTML", 
+//         "CSS", 
+//         "JavaScript", 
+//         "MySQL"
+//       ]
+//     },
+//     {
+//       type: "list",
+//       message: "What is your preferred method of communication?",
+//       name: "contact",
+//       choices: [
+//         "email",
+//         "phone",
+//         "telekinesis"
+//       ]
+//     }
+//   ]).then(function(data) {
+  
+//     var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+  
+//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+  
+//       if (err) {
+//         return console.log(err);
+//       }
+  
+//       console.log("Success!");
+  
+//     });
+//   });
+  
 
 //  console.log(questions[0]);
 function writeToFile(fileName, data) {
 }
 
-function init() {
-
-}
-
-init();
