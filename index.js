58acb04ea09dba658040874c0b89dd28b8739b02
John Kang <pollyfactor@hotmail.com>
2020-03-26 17:13:31 -0400

    added questions array

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const axiodd = require("axios");
 
-const questions = [
-
+const questions = ["what is your Github username?", 
+"What is your project title?", 
+"Please write a description about your project",
+"Would you like a Table of Contents?",
+"What commmand should be used to install dependencies?",
+"What do you want the user know about using this repo?",
+"What commmand should be used to run tests?",
+"What does the user need to know about contributing to the repo?"
 ];
 
+
+//  console.log(questions[0]);
 function writeToFile(fileName, data) {
 }
 
